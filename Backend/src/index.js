b4ecc8f99e47b2f5a76201cf82595cbc62e1c5ba
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-10-11 17:02:01 +0300

    Create index

---
+++ Backend/src/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, console.log(`Server listening on port ${PORT}...`));
