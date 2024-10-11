77fa915b72c34316b5ba5a52838efbdf9bdb5f6e
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-10-11 17:38:10 +0300

    Connect to the DB

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
+
+// Connect to DB
+mongoose.connect("mongodb://localhost:27017/The_Project")
+    .then(() => console.log("DB connected successfully"))
+    .catch(err => console.log("DB error, ", err.message));
 
 app.use(express.urlencoded({ extended: false }));
 
